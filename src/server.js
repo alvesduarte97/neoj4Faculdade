bcb882559fe67495fee32574e494af3834b0d684
Anderson Duarte <andersonduarte@MacBook-Pro-de-Anderson.local>
2020-04-18 20:18:15 -0300

    Inserindo varias entidades com diferentes atributos

--- src/server.js
+++ src/server.js
@@ -1,92 +1,54 @@
-const neo4j = require('neo4j-driver')
+const neo4j = require("neo4j-driver");
+const Person = require("./model/person");
 
-const urlBase = 'bolt://localhost:7687';
-const userBase = 'neo4j';
-const passwordBase = 'faculdade';
-
-const driver = neo4j.driver(urlBase, neo4j.auth.basic(userBase, passwordBase));
+const driver = neo4j.driver(
+  "bolt://localhost:7687",
+  neo4j.auth.basic("neo4j", "faculdade")
+);
 const session = driver.session();
-const personName = 'Alice'
+const personName = "Alice";
+var listPerson = [];
+
+let alice = new Person();
+alice.nmUser = "Alice";
+alice.nrAge = "22";
+alice.dsNickname = "Ally";
+listPerson.push(alice);
+
+let arya = new Person();
+arya.nmUser = "Arya";
+arya.nrAge = "23";
+arya.dsNickname = "Ary";
+listPerson.push(arya);
+
+let maria = new Person();
+maria.nmUser = "Maria";
+maria.nrAge = "51";
+maria.dsNickname = "Dudu";
+listPerson.push(maria);
 
 console.log("Antes");
-runNeo();
+runNeo(listPerson)
 console.log("Depois");
 
- 
-async function runNeo(){
+async function runNeo(listPerson) {
   try {
-    const result = await session.run(
-      'CREATE (a:Person {name: $name}) RETURN a',
-      { name: personName }
-    )
-  
-    const singleRecord = result.records[0]
-    const node = singleRecord.get(0)
-  
-    console.log(node.properties.name)
+    for (person of listPerson) {
+      const result = await session.run(
+        "CREATE (node:Person {name: $name,nickname: $nickname, age: $age}) RETURN node",
+        { name: person.nmUser, nickname: person.dsNickname,age: person.nrAge }
+      );
+
+      const singleRecord = result.records[0];
+      const node = singleRecord.get(0);
+
+      console.log("NODE:" ,node.properties);
+    }
   } finally {
-    await session.close()
+    await session.close();
   }
-  
+
   // on application exit:
-  await driver.close()
+  await driver.close();
 }
-
-
-
-
-
-// const neo4j = require("neo4j-driver");
-// const Person = require("./model/person");
-
-// const driver = neo4j.driver(
-//   "bolt://localhost:7687",
-//   neo4j.auth.basic("neo4j", "faculdade")
-// );
-// const session = driver.session();
-// const personName = "Alice";
-// var listPerson = [];
-
-// let alice = new Person();
-// alice.nmUser = "Alice";
-// alice.nrAge = "22";
-// alice.dsNickname = "Ally";
-// listPerson.push(alice);
-
-// let arya = new Person();
-// arya.nmUser = "Arya";
-// arya.nrAge = "23";
-// arya.dsNickname = "Ary";
-// listPerson.push(arya);
-
-// let maria = new Person();
-// maria.nmUser = "Maria";
-// maria.nrAge = "51";
-// maria.dsNickname = "Dudu";
-// listPerson.push(maria);
-
-// console.log("Antes");
-// runNeo(listPerson)
-// console.log("Depois");
-
-// async function runNeo(listPerson) {
-//   try {
-//     for (person in listPerson) {
-//       const result = await session.run(
-//         "CREATE (node:Person {name: $name,nickname: $nickname, age: $age}) RETURN node",
-//         { name: person.nmUser, nickname: person.dsNickname,age: person.nrAge }
-//       );
-
-//       const singleRecord = result.records[0];
-//       const node = singleRecord.get(0);
-
-//       console.log(node.properties.name);
-//     }
-//   } finally {
-//     await session.close();
-//   }
-
-//   // on application exit:
-//   await driver.close();
-// }
 
